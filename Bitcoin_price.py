579efa482415ef8fa78ee2080d1f84b60fe81c04
Aleksandar Kostadinov <aemilov01@gmail.com>
2022-12-19 13:57:54 +0200

    Mini projects tasks commit

---
+++ Bitcoin_price.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+URL = 'https://coinmarketcap.com/currencies/bitcoin/'
+req = requests.get(URL)
+
+soup = bs(req.content, 'html.parser')
+
+price = soup.find('div', {'class': 'priceValue'})
+extract = price.select_one('span')
+
+print(f'Bitcoin price today is {extract.text} per (BTC / USD)')
+
+
